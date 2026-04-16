77dc70bbdbdbec2365588557703c071e93ece3e7
Grace <grace6424@icloud.com>
2026-04-16 10:18:19 +0200

    Implement routing with react-router, add MainLayout, Home, About, NotFound pages, and NavBar/Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { Home, About, NotFound } from "./pages";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">React + TailwindCSS</h1>
-
-      <div className="avatar">
-        <div className="w-24 rounded-xl">
-          <img
-            src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp"
-            alt="blondeLady"
-          />
-        </div>
-      </div>
-      <div className="avatar">
-        <div className="w-24 rounded-full">
-          <img
-            src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
-            alt="kitty"
-          />
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
